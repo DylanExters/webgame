f3cd079543e1d851905e18d8010c86b497c142dd
DylanExters <144001877+DylanExters@users.noreply.github.com>
2024-02-08 12:51:49 +0100

    made switches and undragable items

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ const gamewindow = document.getElementById("gameWindow");
 const character = document.getElementById("character");
 const offsetCharacter = 16;
 
+const door1 = document.getElementById("door1");
+const sign = document.getElementById("sign");
+
 gamewindow.onclick = function (e) {
     var rect = gamewindow.getBoundingClientRect();
     var x = e.clientX - rect.left;
@@ -14,7 +17,23 @@ gamewindow.onclick = function (e) {
     character.style.left = x - offsetCharacter + "px";
     character.style.top = y - offsetCharacter + "px";
 
-    if(e.target.id == "door1"){
-        console.log("You need a key");
+    switch(e.target.id) {
+        case "door1":
+            door1.style.opacity = 0.5;
+            sign.style.opacity = 0.8;
+        break;
+
+        case "sign":
+            sign.style.opacity = 0.5;
+            door1.style.opacity = 0.8;
+        break;
+
+        default:
+            //explode
+            door1.style.opacity = 0.8;
+            sign.style.opacity = 0.8;
+
+            break;
     }
+
 }
