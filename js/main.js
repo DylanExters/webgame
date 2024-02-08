53ab3f192beff53a32982f0983065897b0aca6c5
DylanExters <144001877+DylanExters@users.noreply.github.com>
2024-02-08 12:26:07 +0100

    added character moveing and a door

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-document.getElementById("title").innerText = "Point and click"
+// game window reference
+const gamewindow = document.getElementById("gameWindow");
+
+// main character
+const character = document.getElementById("character");
+const offsetCharacter = 16;
+
+gamewindow.onclick = function (e) {
+    var rect = gamewindow.getBoundingClientRect();
+    var x = e.clientX - rect.left;
+    var y = e.clientY - rect.top;
+
+    console.log(e.target.id);
+    character.style.left = x - offsetCharacter + "px";
+    character.style.top = y - offsetCharacter + "px";
+
+    if(e.target.id == "door1"){
+        console.log("You need a key");
+    }
+}
